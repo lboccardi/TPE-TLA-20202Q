710d70b8ab9be32590e5a7bbd2d47219807127f6
Euge <epineiro@itba.edu.ar>
2020-11-23 12:43:56 -0300

    feat compiler started

---
+++ compiler.c
@@ -0,0 +1,12 @@
+/* El compiler recibe una dirección donde está el archivo para leer el programa (file.clux)
+    fopen
+    read (file.clux)
+    write (file.asm)
+    Tiene que devolver como lenguaje de salida ASM en otro file (file.asm)
+    Proveer un mecanismo de entrada de datos: STDIN.
+    Proveer un mecanismo de salida de datos:  STDOUT.
+ */
+
+int main(int argc, char *argv[])
+{
+}
