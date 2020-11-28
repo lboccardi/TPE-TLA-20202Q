1a217a018dde6cee8eb22c0a09e4e2237cfbab12
Euge <epineiro@itba.edu.ar>
2020-11-28 16:26:14 -0300

    added swap arrays example

--- intermediate.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int main(){ int array[12]={1,2,3,4,5,6,7,8,9,10,11,12}; int len=0; while(len < 12){printf("array %d = %d \n",len,array[len]);
-len=len+1; }
-int a[3]; a[0]=10; a[1]=20; a[2]=30; printf("%d %d %d",a[0],a[1],a[2]);
-  }
